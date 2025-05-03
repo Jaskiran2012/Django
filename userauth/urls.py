b1c5825aaf5e133ded61de3d9c11a357a83cedd7
Jaskiran2012 <jaskirankaur187@gmail.com>
2025-05-03 16:21:48 +0530

    final evaluation

--- userauth/urls.py
+++ userauth/urls.py
@@ -11,7 +11,7 @@ urlpatterns = [
     path('add-employee/', views.add_employee_view, name='add_employee'),
     path('choose-mode/', views.choose_mode_view, name='choose-mode'),
     path('choose-role/', views.choose_role_view, name='choose-role'),
-    path('login/', views.login_view, name='login'),
+    path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(next_page='login'), name='logout'),
     
     # Dashboard URLs
